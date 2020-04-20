e44612e75df56e25fd8e8121bff6ac35145feca8
prade091078 <prade.accet@gmail.com>
2020-04-21 01:51:51 +0530

    first commit

---
+++ app/src/main/java/com/example/manirathnam/app_1viewto2view/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.manirathnam.app_1viewto2view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by manirathnam on 22-11-2017.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
